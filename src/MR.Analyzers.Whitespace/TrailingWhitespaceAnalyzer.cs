f209c0385956d89eaab59c727c6a559611447bbc
Mohammad Rahhal <m.r992@hotmail.com>
2018-12-21 14:55:38 +0200

    Bound error to only whitespace in single line comments

--- src/MR.Analyzers.Whitespace/TrailingWhitespaceAnalyzer.cs
+++ src/MR.Analyzers.Whitespace/TrailingWhitespaceAnalyzer.cs
@@ -1,9 +1,9 @@
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Text;
 
 namespace MR.Analyzers.Whitespace
 {
@@ -21,26 +21,38 @@ namespace MR.Analyzers.Whitespace
 		private void AnalyzeSyntaxTree(SyntaxTreeAnalysisContext context)
 		{
 			var root = context.Tree.GetRoot();
-			var uselessWhitespaceTriviaList = new List<SyntaxTrivia>();
+
+			// Trailing whitespace
 
 			var trailingWhitespace = root.DescendantTrivia()
 				.Where(t => t.IsKind(SyntaxKind.EndOfLineTrivia))
 				.Where(endOfLineTrivia => endOfLineTrivia.Token.TrailingTrivia.Count > 1)
 				.Select(endOfLineTrivia => endOfLineTrivia.Token.TrailingTrivia[0]);
-			uselessWhitespaceTriviaList.AddRange(trailingWhitespace);
+
+			foreach (var trivia in trailingWhitespace)
+			{
+				context.ReportDiagnostic(Diagnostic.Create(
+					WhitespaceDiagnosticDescriptors.WS1000_TrailingWhitespace,
+					trivia.GetLocation()));
+			}
+
+			// Single line comments
 
 			var singleLineCommentsWithTrailingWhitespace = root.DescendantTrivia()
 				.Where(t => t.IsKind(SyntaxKind.SingleLineCommentTrivia))
 				.Where(t => t.ToString().EndsWith(" "))
 				.ToList();
 
-			uselessWhitespaceTriviaList.AddRange(singleLineCommentsWithTrailingWhitespace);
-
-			foreach (var trivia in uselessWhitespaceTriviaList)
+			foreach (var trivia in singleLineCommentsWithTrailingWhitespace)
 			{
+				var str = trivia.ToString();
+				var trimmed = str.TrimEnd();
+				var span = TextSpan.FromBounds(trivia.SpanStart + trimmed.Length, trivia.Span.End);
+				var location = Location.Create(context.Tree, span);
+
 				context.ReportDiagnostic(Diagnostic.Create(
 					WhitespaceDiagnosticDescriptors.WS1000_TrailingWhitespace,
-					trivia.GetLocation()));
+					location));
 			}
 		}
 	}
