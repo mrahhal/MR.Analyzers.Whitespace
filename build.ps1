f1a3b20b32c5a8e5082f247b1f64e8bcb6196c55
Mohammad Rahhal <m.r992@hotmail.com>
2021-11-25 16:35:25 +0200

    Update build

--- build.ps1
+++ build.ps1
@@ -36,7 +36,7 @@ foreach ($srcProject in $srcProjects) {
 ExitIfFailed
 
 foreach ($testProject in $testProjects) {
-	dotnet test $testProject --no-restore
+	dotnet test $testProject --no-restore -verbosity:diagnostic
 }
 ExitIfFailed
 
