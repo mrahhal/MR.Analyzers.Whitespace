f48ef1c862000d57031eec2b6331ba00679f67e4
Mohammad Rahhal <m.r992@hotmail.com>
2021-11-25 15:36:19 +0200

    Update build

--- build.ps1
+++ build.ps1
@@ -13,17 +13,31 @@ function CreateStamp() {
 	return ([long]([DateTime]::UtcNow - (New-Object DateTime 2020, 1, 1)).TotalSeconds).ToString().PadLeft(11, '0')
 }
 
-if (Test-Path artifacts) {
+if (Test-Path "artifacts") {
 	Remove-Item -Recurse artifacts
 }
 
-dotnet restore
+# Resolve src/test projects
+
+$srcProjects = @(Get-ChildItem -Path ./src/*/*.csproj -File)
+$testProjects = @(Get-ChildItem -Path ./test/*/*.csproj -File)
+$allProjects = $srcProjects + $testProjects
+
+# ---
+
+foreach ($project in $allProjects) {
+	dotnet restore $project
+}
 ExitIfFailed
 
-dotnet build --no-restore -c Release
+foreach ($srcProject in $srcProjects) {
+	dotnet build $srcProject --no-restore -c Release
+}
 ExitIfFailed
 
-dotnet test --no-restore
+foreach ($testProject in $testProjects) {
+	dotnet test $testProject --no-restore
+}
 ExitIfFailed
 
 $versionSuffixArg = "";
@@ -32,4 +46,6 @@ if (Test-Path env:GITHUB_ACTIONS) {
 	$versionSuffixArg = "--version-suffix dev-$(CreateStamp)"
 }
 
-Invoke-Expression "dotnet pack --no-restore -c Release -o artifacts/packages $versionSuffixArg"
+foreach ($srcProject in $srcProjects) {
+	Invoke-Expression "dotnet pack $srcProject --no-restore -c Release -o artifacts/packages $versionSuffixArg"
+}
